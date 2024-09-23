f756c1555693b6c8178644f8a58076986018a6bc
faoroj <jfaoro15@gmail.com>
2024-09-23 17:39:00 -0400

    GroupVisitForm Update

--- FossilDiscoveryLab/src/components/groupvisit/GroupVisitForm.jsx
+++ FossilDiscoveryLab/src/components/groupvisit/GroupVisitForm.jsx
@@ -6,9 +6,27 @@ const GroupVisitForm = () => {
 
       {GroupForm.map((form, index) => (
         <div key={form.id} className='flex flex-col mb-7'>
-          <p className='text-bold-16'>{form.header}</p>
-          <div className={index === 8 ? 'w-full h-[170px] border rounded-[6px] mt-4 border-black' : 'border-black border w-[630px] h-[45px] rounded-[6px] mt-4'}></div>
-          
+
+          <p className='text-bold-16'>
+            {form.header.slice(0, -1)}
+            <span className="text-red-500">{form.header.slice(-1)}</span>
+          </p>
+
+          {index === 1 && (
+            <div className='flex'>
+              <div className='w-[300px] h-[45px] border rounded-[6px] mt-4 border-black mr-[30px]'>
+                <p className='text-normal-16 py-2 ml-3'>First</p>
+              </div>
+              <div className='w-[300px] h-[45px] border rounded-[6px] mt-4 border-black'>
+              <p className='text-normal-16 py-2 ml-3'>Last</p>
+              </div>
+            </div>
+            )}
+
+          {index != 1 && (  
+            <div className={index === 8 ? 'w-full h-[170px] border rounded-[6px] mt-4 border-black' : 'border-black border w-[630px] h-[45px] rounded-[6px] mt-4'}></div>
+          )}
+
           {form.icon && <img src={form.icon} alt={`${form.title} icon`} 
           className='w-[30px] h-[30px] mt-1 cursor-pointer'/>}
         </div>   
