c0afe5534dd5c2aa3b8fe31e383af6b0db53ac00
faoroj <jfaoro15@gmail.com>
2024-09-23 22:52:31 -0400

    Nav Bar Dropdown Setup

--- FossilDiscoveryLab/src/components/nav/Navbar.jsx
+++ FossilDiscoveryLab/src/components/nav/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
         </div>
       </div>
 
-      <div className='h-[44px] flexBetween bg-white'>
+      <div className='h-[44px] flexBetween bg-white relative'>
 
         <ul className="list-none custom:flex hidden pl-[100px]">
           {navLinks.map((nav, index) => (
@@ -45,9 +45,13 @@ const Navbar = () => {
               className={`text-bold-16 cursor-pointer text-center mt-[10px] ${
                 active === nav.title ? "text-secondary" : "text-White"
               } ${index === navLinks.length - 1 ? "mr-0" : "mr-6 md:mr-10"} hover:text-secondary`}
-              onClick={() => setActive(nav.title)}
+              onClick={nav.title === 'Visit' ? handleVisitClick : () => setActive(nav.title)}
             >
-              <Link to={nav.path}>{nav.title}</Link>
+              {nav.title !== 'Visit' ? (
+                <Link to={nav.path}>{nav.title}</Link>
+                ) : (
+                <p>{nav.title}</p>
+              )}
 
               <hr
                 className={`w-[60px] h-[3px] rounded-[6px] mt-2 transition-all duration-300 ${
@@ -56,9 +60,22 @@ const Navbar = () => {
               />
 
             </li>
-            
+
           ))}
         </ul>
+
+        {isDropdownOpen && (
+        <div className='absolute top-full left-[235px] w-[190px] h-[186px] px-[14px] flex justify-center items-center flex-col bg-secondary z-50'>
+          {visitLinks.map((nav, index) => (
+            <Link to={nav.path}>
+            <h1 key={index} className={`text-semibold-16 text-flat hover:text-black cursor-pointer ${index === 0 ? "mt-[0px]" : "mt-[12px]"}`}>
+              {nav.title}
+            </h1>
+            </Link>
+          ))}
+        </div>
+      )}
+
 
         <div className='flexCenter pr-[100px]'>
           <p className='flexEnd text-normal-16 mr-2'>
