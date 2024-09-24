2b7511a2407f973e36bcb7686df79430d11df8c9
faoroj <jfaoro15@gmail.com>
2024-09-23 20:59:39 -0400

    Donate Screen Update

--- FossilDiscoveryLab/src/components/home/Donate.jsx
+++ FossilDiscoveryLab/src/components/home/Donate.jsx
@@ -7,19 +7,19 @@ const Donate = () => {
       <img 
         src={DonateBanner} 
         alt='Exhibit' 
-        className='flex-shrink-0 self-stretch w-full h-[542px] object-cover'
+        className='flex-shrink-0 self-stretch w-full h-[420px] object-cover'
       />
 
-      <div className='bg-[rgba(217,217,217,0.7)] absolute w-[589px] h-[240px] inset-0 top-[156px] left-[404px]'>
+      <div className='bg-[rgba(217,217,217,0.7)] absolute w-[500px] h-[220px] inset-0 top-[100px] left-[404px]'>
 
-        <h1 className='text-semibold-28 pt-6 px-[46px]'>Want To Support Us?</h1>
+        <h1 className='text-semibold-21 pt-6 px-[46px]'>Want To Support Us?</h1>
         
         <p className='text-normal-18 text-mediumBlack pt-3 px-[46px]'>
           Donations allow us to continue building new exhibits, develop new education programs and more.
         </p>
 
         <div className='flexCenter'>
-          <button className='custom__button__bevel custom__button__medium mb-[10px] mt-6'>Donate</button>
+          <button className='custom__button__bevel custom__button__medium mb-[15px]'>Donate</button>
         </div>
 
       </div>
