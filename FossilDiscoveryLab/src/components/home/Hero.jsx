c4a49ec37587a4a4c11cd9e363fe29f3e421e6ea
faoroj <jfaoro15@gmail.com>
2024-09-25 17:45:12 -0400

    Hero Section Responsiveness

--- FossilDiscoveryLab/src/components/home/Hero.jsx
+++ FossilDiscoveryLab/src/components/home/Hero.jsx
@@ -3,20 +3,26 @@ import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
-    <section 
-      id="home" 
-      className={`flex md:flex-row flex-col relative`}>
+    <section id="home" className={`flex md:flex-row flex-col relative`}>
 
       <img src={hero} alt='hero' className='flex-shrink-0 self-stretch w-full h-screen object-cover'/>
 
       
       <div className='padding absolute inset-0 flexCenter text-center flex-col'>
-        
-        <div className='flexCenter'>
-          <img src={pointer} alt="pointer" className='w-[39px] h-[39px] mr-3 border-none'/> 
-          <h1 className='text-bold-48 text-flat'>Welcome to The <span className='text-secondary'>Fossil</span> Discovery Lab</h1>
+
+        <div className='flexCenter relative'>
+          <h1 className='text-bold-48 text-flat relative'>
+            <span className="relative">
+              <img 
+                src={pointer} 
+                alt="pointer" 
+                className='w-[39px] h-[39px] absolute left-[-50px] top-[32px] transform -translate-y-1/2' 
+              /> 
+              Welcome to The <span className='text-secondary'>Fossil</span> Discovery Lab
+            </span>
+          </h1>
         </div>
-        
+
         <h2 className='text-medium-28 text-flat'>Step Into a World Lost in Time</h2>
 
         <div className='flexCenter flex-col sm:flex-row mt-14'>
