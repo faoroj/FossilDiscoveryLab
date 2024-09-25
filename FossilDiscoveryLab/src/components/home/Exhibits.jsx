1a56ad338b39df05b8687fc6be198dc8b6cb37ac
faoroj <jfaoro15@gmail.com>
2024-09-25 18:37:23 -0400

    Exhibits Responsiveness (WIP)

--- FossilDiscoveryLab/src/components/home/Exhibits.jsx
+++ FossilDiscoveryLab/src/components/home/Exhibits.jsx
@@ -1,22 +1,24 @@
 import { exhibits } from '../../constants';
 
 const ExhibitCard = ({ image, title, content, }) => (
-  <div className='flex flex-col'>
+  <div className='flex flex-col w-full max-w-full'>
 
-    <div className='w-[470px]'>
-      <img src={image} alt="exhibit" className="w-full h-full object-contain" />
+    <div className='w-full'>
+      <img src={image} alt="exhibit" className="w-full h-full object-cover" />
     </div>
 
-    <div className="bg-white pt-[30px] flex flex-col h-[300px]">
-      <h3 className="text-bold-21 text-mediumBlack max-w-[470px] px-[26px]">
+    <div className="bg-white pt-6 flex flex-col h-[300px] flex-grow">
+
+      <h3 className="text-bold-21 text-mediumBlack px-6">
         {title}
       </h3>
-      <p className="text-normal-16 text-mediumBlack max-w-[470px] px-[26px] mt-3">
+
+      <p className="text-normal-16 text-mediumBlack px-6 mt-3">
         {content}
       </p>
 
-      <div className='mt-auto mb-3 px-[26px]'>
-        <button type='button' className='custom__button__open '>Learn More</button>
+      <div className='mt-auto mb-3 px-6'>
+        <button type='button' className='custom__button__open mt-8'>Learn More</button>
       </div>
 
     </div>
@@ -28,18 +30,18 @@ const Exhibits = () => {
   return (
     <section className='max-container'>
 
-      <div className='flexBetween'>
-        <h1 className='font-inter text-[38px] font-semibold leading-[150%]'>Explore our Exhibits</h1>
-        <p className='text-semibold-18 mr-5 cursor-pointer hover:underline'>View All &gt;</p>
+      <div className='flexBetween '>
+        <h1 className='font-inter text-[22px] lg:text-[38px] md:text-[28px] sm:text-[22px] font-semibold leading-[150%]'>Explore our Exhibits</h1>
+        <p className='text-semibold-16 md:text-semibold-18 mr-5 cursor-pointer hover:underline'>View All &gt;</p>
       </div>
 
-      <div className='flexCenter mt-9'>
-        <div className='flexCenter gap-x-[80px]'>
-          {exhibits.map((exhibit, index) => (
-            <ExhibitCard key={exhibit.id} {...exhibit} exhibits={exhibits} index={index}/>
-          ))}
-        </div>
+      
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-[150px] mt-10 justify-items-center'>
+        {exhibits.map((exhibit, index) => (
+          <ExhibitCard key={exhibit.id} {...exhibit} exhibits={exhibits} index={index}/>
+        ))}
       </div>
+  
 
     </section>
   )
