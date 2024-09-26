656ff70849722fe021049023481988ae8c7f32d4
faoroj <jfaoro15@gmail.com>
2024-09-26 05:49:41 -0400

    Learn More Responsiveness

--- FossilDiscoveryLab/src/components/home/Learn.jsx
+++ FossilDiscoveryLab/src/components/home/Learn.jsx
@@ -4,20 +4,21 @@ import { Link } from 'react-router-dom';
 const Learn = () => {
   return (
     <section 
-    id='learn-more'
-    className='max-container'>
+      id='learn-more'
+      className='max-container'
+    >
 
-      <h1 className='font-inter text-[38px] font-semibold leading-[150%]'>
+      <h1 className='font-inter text-[32px] md:text-[48px] font-semibold leading-[150%]'>
         Learn More
       </h1>
 
-      <div className='flex flex-row justify-around items-start mt-[50px]'>
+      <div className='grid md:grid-cols-3 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 sm:gap-[64px] gap-12 mt-[50px]'>
         
           {learnmore.map((learn) => (
 
-            <div key={learn.id} className='flex flex-col h-[380px]'>
+            <div key={learn.id} className='flex flex-col h-[460px] sm:h-[550px] custom:h-[530px] md:h-[520px] lg:h-[480px]'>
               <div className='w-full'>
-                <img src={learn.image} alt={learn.title} className='w-[250px] h-[190px] object-cover'/>
+                <img src={learn.image} alt={learn.title} className='w-[360px] h-[280px] object-cover'/>
               </div>
 
               <h3 className="text-bold-21 mt-3">
--- FossilDiscoveryLab/src/pages/Home.jsx
+++ FossilDiscoveryLab/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const App = () => {
         <Learn />
       </section>
 
-      <section>
+      <section className="">
         <Donate />
       </section>
 
