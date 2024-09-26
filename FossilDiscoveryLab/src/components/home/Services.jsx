eb960637f7cad01f1f9db0433d34551a23a28c8b
faoroj <jfaoro15@gmail.com>
2024-09-25 20:46:12 -0400

    Services Responsiveness

--- FossilDiscoveryLab/src/components/home/Services.jsx
+++ FossilDiscoveryLab/src/components/home/Services.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom';
 
 const Services = () => {
   return (
-    <section className='relative w-full'>
+    <section id="services" className='relative w-full'>
       
-      <div className='relative'>
+      <div className='relative w-full'>
 
-        <img src={Divider} alt="divider" className="flex-shrink-0 self-stretch w-full h-[273px] object-cover" />
+        <img src={Divider} alt="divider" className="w-full h-[273px] object-cover" />
 
         <div className='padding absolute inset-0 flexCenter text-center'>
           <h1 className='text-[32px] md:text-[48px] ss:text-[42px] font-bold font-inter leading-[150%] text-flat max-w-[800px]'>
@@ -19,10 +19,8 @@ const Services = () => {
 
       </div>
  
+      <div className='max-container px-8 sm:px-0 grid grid-cols-1 sm:grid-cols-2'>  
 
-      
-      
-      <div className='flexStart max-container'>   
         <div className='mt-[29px]'>
           {services.map((service) => (
           <div key={service.id} className='mt-7'>
@@ -30,7 +28,7 @@ const Services = () => {
                 {service.title}
               </h3>
               
-            <p className="text-normal-18 text-mediumBlack max-w-[523px] mt-4">
+            <p className="text-normal-18 text-mediumBlack max-w-[623px] mt-4">
               {service.content.split("help@fossildiscoverymuseum.org").map((part, index) => (
                 <>
                   {part}
@@ -42,8 +40,6 @@ const Services = () => {
                 </>
               ))}
             </p>
-
-
           </div>
           ))}
 
@@ -53,12 +49,12 @@ const Services = () => {
             </div>
           </Link>
           
-       </div>
+        </div>
 
        
-       <div>
-        <img src={Assistance} alt="assistance" className='w-[685px] absolute ml-10 top-[200px]'/>
-       </div>
+        <div className='flex justify-center mt-10 sm:mt-0'>
+          <img src={Assistance} alt="assistance" className='w-full h-auto max-w-full object-contain'/>
+        </div>
        
 
       </div>
