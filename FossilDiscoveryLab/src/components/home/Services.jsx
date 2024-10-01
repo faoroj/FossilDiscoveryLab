bd46bc544b457f4dc6283a36052c99a0a7ea848c
faoroj <jfaoro15@gmail.com>
2024-10-01 03:42:37 -0400

    services page animation

--- FossilDiscoveryLab/src/components/home/Services.jsx
+++ FossilDiscoveryLab/src/components/home/Services.jsx
@@ -1,13 +1,20 @@
 import { services } from '../../constants';
 import {  Assistance, Divider } from '../../assets';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Services = () => {
   return (
     <section id="services" className='relative w-full'>
       
       {/*Divider Image */}
-      <div className='relative w-full'>
+      <motion.div 
+        className='relative w-full'
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        viewport={{ once: true }}
+        transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.1 }}
+      >
         <img src={Divider} alt="divider" className="w-full h-[150px] xs:h-[273px] object-cover" />
 
         <div className='padding absolute inset-0 flexCenter text-center'>
@@ -16,13 +23,20 @@ const Services = () => {
             &nbsp;to serve the <span className="text-secondary">needs</span> of visitors.
           </h1>
         </div>
-      </div>
+      </motion.div>
  
 
       <div className='max-container padding-x grid grid-cols-1 custom:grid-cols-2 '>  
-        <div className='mt-0 custom:mt-[29px] order-last custom:order-first'>
-          {services.map((service) => (
-          <div key={service.id} className='mt-7'>
+        <motion.div className='mt-0 custom:mt-[29px] order-last custom:order-first'>
+          {services.map((service, index) => (
+          <motion.div 
+            key={service.id} 
+            initial={{ opacity: 0, y: 120 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.4, delay:1 + (index * .3) }}
+            className='mt-7'
+          >
               <h3 className="text-bold-21 max-w-[523px]">
                 {service.title}
               </h3>
@@ -39,21 +53,32 @@ const Services = () => {
                 </>
               ))}
             </p>
-          </div>
+          </motion.div>
           ))}
 
           <Link to="/Accessibility">
-            <div>
+            <motion.div
+              initial={{ opacity: 0, y: 120 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: true }}
+              transition={{ duration: 0.4, delay:2 }}
+            >
               <button type='button' className='custom__button__open mt-8'>Accessibility</button>
-            </div>
+            </motion.div>
           </Link>
           
-        </div>
+        </motion.div>
 
        
-        <div className='flex justify-center  sm:mt-0'>
+        <motion.div 
+          className='flex justify-center  sm:mt-0'
+          initial={{opacity: 0}}
+          whileInView={{opacity: 1}}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, ease: 'easeInOut', delay: 0.6 }}
+        >
           <img src={Assistance} alt="assistance" className='w-full h-[auto] max-w-[600px] -mt-3 xxs:-mt-8 xs:-mt-16 sm:-mt-18 object-contain z-10'/>
-        </div>
+        </motion.div>
        
 
       </div>
