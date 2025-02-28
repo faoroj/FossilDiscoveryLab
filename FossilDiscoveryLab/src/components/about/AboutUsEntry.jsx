d58fa10bc957da2f4b03360e739932d76c2f30b8
faoroj <jfaoro15@gmail.com>
2025-02-27 22:05:53 -0500

    update

--- FossilDiscoveryLab/src/components/about/AboutUsEntry.jsx
+++ FossilDiscoveryLab/src/components/about/AboutUsEntry.jsx
@@ -18,7 +18,7 @@ const AboutUsEntry = () => {
             <p className='text-normal-16 font-inter font-normal mt-[30px]'>
                 At the Fossil Discovery Lab, we are passionate storytellers of the Earth's ancient history. 
                 Founded by a team of dedicated paleontologists and enthusiasts, we strive to create an immersive 
-                experience that transports visitors back in time to when dinosaurs roamed the planet. Our museum 
+                experience that transports visitor's back in time to when dinosaurs roamed the planet. Our museum 
                 is a treasure trove of remarkable fossils, each with its own tale of survival, adaptation, and extinction. 
                 Whether you're a seasoned fossil enthusiast or a curious explorer, join us on a journey through millions of years, 
                 where you'll uncover the wonders of paleontology and the awe-inspiring world of dinosaurs.
